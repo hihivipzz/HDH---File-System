2268696aec1a250bf09ff594e23c05fd054c370b
20120480 <hihivipzz2002@gmail.com>
2022-11-06 14:28:29 +0700

    Class entry

--- Volume/Entry.h
+++ Volume/Entry.h
@@ -1,5 +1,21 @@
 #pragma once
 class Entry
 {
+private:
+	uint16_t size;
+	char nameSize;
+	char passSize;
+	uint16_t timeCreate;
+	uint16_t dateCreate;
+	char type;
+	uint16_t startCluster;
+	int dataSize;
+	string name;
+	string password;
+	char* data;
+
+public:
+	void createEntry(string name, string password, int dataSize, int type, uint16_t startCluster);
+	char* toBytes();
 };
 
